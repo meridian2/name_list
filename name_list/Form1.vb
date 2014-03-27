f5976addb4d71fd8da8e748321d40d6ee50528ba
meridian2 <meridian1@live.com.au>
2014-03-27 12:25:33 +0800

    initial_upload
    upload1

---
+++ name_list/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        ' hey guys, just copy and paste the below line, and replace my name with yours :)
+        ListBox1.Items.Add("Alan")
+
+
+    End Sub
+
+End Class
